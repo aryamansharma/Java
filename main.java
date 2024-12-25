949652456d7b4467f39cb0cf6861e146aa349880
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2024-12-26 00:49:19 +0530

    static keyword

--- main.java
+++ main.java
@@ -1,33 +1,23 @@
-class Student {
-    int rollNo;
-    String name;
-    int marks;
+class Phone {
+    String brand;
+    int price;
+    static String type;
+
+    public static void show(Phone obj) {
+        System.out.println(obj.brand+": "+obj.price+": "+type);
+    }
 }
 
 class Main {
     public static void main(String a[]) {
-        Student s1 = new Student();
-        s1.rollNo = 1;
-        s1.name="ABC";
-        s1.marks=89;
-
-        Student s2 = new Student();
-        s2.rollNo = 2;
-        s2.name="EFG";
-        s2.marks=79;
-
-        Student s3 = new Student();
-        s3.rollNo = 3;
-        s3.name="PQR";
-        s3.marks=89;
-
-        Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
-        
-        for(Student student : students) {
-            System.out.println(student.name+" : "+student.marks);
-        }
+          Phone.type = "Phone";
+          Phone obj1 = new Phone();
+          obj1.brand = "Apple";
+          obj1.price = 1500;
+          Phone obj2 = new Phone();
+          obj2.brand = "Samsung";
+          obj2.price = 1800;
+          Phone.show(obj1);
+          Phone.show(obj2);
     }
 }
