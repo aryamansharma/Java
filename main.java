b3044d21b5dda42f7d3dc52738847140245291a9
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2024-12-28 14:41:32 +0530

    inheritance and packages

--- main.java
+++ main.java
@@ -1,23 +1,18 @@
-class Phone {
-    String brand;
-    int price;
-    static String type;
+class A {
+    public void show() {
+        System.err.println("in A");
+    } 
+}
 
-    public static void show(Phone obj) {
-        System.out.println(obj.brand+": "+obj.price+": "+type);
-    }
+class B {
+    public void show() {
+        System.err.println("in B");
+    } 
 }
 
 class Main {
     public static void main(String a[]) {
-          Phone.type = "Phone";
-          Phone obj1 = new Phone();
-          obj1.brand = "Apple";
-          obj1.price = 1500;
-          Phone obj2 = new Phone();
-          obj2.brand = "Samsung";
-          obj2.price = 1800;
-          Phone.show(obj1);
-          Phone.show(obj2);
+
+        
     }
 }
