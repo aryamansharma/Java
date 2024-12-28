e26a1df2c1f147ad65f17582f28483509a27d78d
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2024-12-28 16:44:38 +0530

    wrapper class and access modifiers

--- main.java
+++ main.java
@@ -1,18 +1,5 @@
-class A {
-    public void show() {
-        System.err.println("in A");
-    } 
-}
-
-class B {
-    public void show() {
-        System.err.println("in B");
-    } 
-}
-
 class Main {
     public static void main(String a[]) {
 
-        
     }
 }
