bcd9a1782e74c5fcead78ca024ba04082527ce50
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2024-12-31 17:53:24 +0530

    small quiz project

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        
+
+        QuestionService questionService = new QuestionService();
+        questionService.playQuiz();
     }
 }
