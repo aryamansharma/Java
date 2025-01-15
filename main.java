fcfccdc9ed1aa1ddad880eeb86276a2b3d358d46
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2025-01-15 19:11:44 +0530

    lambda expression

--- main.java
+++ main.java
@@ -1,15 +1,12 @@
 @FunctionalInterface
 interface A {
-    void show();
+    int add(int i, int j);
 }
 
 class Main {
     public static void main(String a[]) {
-        A obj = new A() {
-            public void show() {
-                System.err.println("in A interface show");
-            }
-        };
-        obj.show();
+        A obj = (i, j) -> i  + j;
+        int result = obj.add(5,99);
+        System.err.println(result);
     }
 }
